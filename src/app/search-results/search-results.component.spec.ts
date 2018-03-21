688a535eaccf5e1e614e73691e5836f09032ea28
Hugo Noro <hnoro2@sapient.com>
2018-03-21 01:39:49 +0000

    Implemented the logic to append the new search result to the array of results and corresponding unit test

--- src/app/search-results/search-results.component.spec.ts
+++ src/app/search-results/search-results.component.spec.ts
@@ -1,25 +1,51 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchResultsComponent } from './search-results.component';
+import { Component, ViewChild } from '@angular/core';
+
+import { ForecastResult } from '../core/models/forecast';
+
+@Component({
+    template: '<app-search-results [result]="forecastResult"></app-search-results>'
+})
+class TestHostComponent {
+    @ViewChild(SearchResultsComponent) searchResultsComponent: SearchResultsComponent;
+    forecastResult: ForecastResult;
+}
 
 describe('SearchResultsComponent', () => {
-  let component: SearchResultsComponent;
-  let fixture: ComponentFixture<SearchResultsComponent>;
+    let component: TestHostComponent;
+    let fixture: ComponentFixture<TestHostComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SearchResultsComponent ]
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [SearchResultsComponent, TestHostComponent]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TestHostComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should append the the search result to the results array', () => {
+        component.forecastResult = {
+            'city': 'Paris',
+            'country': 'FR',
+            'dayForecast': [{
+                'time': '03:00 AM',
+                'temperature': 10
+            }]
+        };
+
+        fixture.detectChanges();
+
+        expect(component.searchResultsComponent.searchResults.length).toBe(1);
     })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SearchResultsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ForecastResult } from '../core/models/forecast';
 
 @Component({
-  selector: 'app-search-results',
-  templateUrl: './search-results.component.html',
-  styleUrls: ['./search-results.component.scss']
+    selector: 'app-search-results',
+    templateUrl: './search-results.component.html',
+    styleUrls: ['./search-results.component.scss']
 })
-export class SearchResultsComponent implements OnInit {
+export class SearchResultsComponent implements OnInit, OnChanges {
 
-  constructor() { }
+    @Input() result: ForecastResult;
 
-  ngOnInit() {
-  }
+    searchResults: Array<ForecastResult> = [];
 
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.result && changes.result.currentValue) {
+            this.searchResults = [...this.searchResults, this.result];
+        }
+    }
 }
